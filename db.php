a8d76573377a8cb405bc64d22dc16668862b42e3
udara <kumuduelkaduwa@gmail.com>
2024-10-23 13:52:46 +0530

    fixed

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "db";  // Use the service name defined in the Docker Compose file
-$username = "root";   // Use the same username defined in your Docker Compose
+$username = "user";   // Use the same username defined in your Docker Compose
 $password = "1234";   // Use the password defined in your Docker Compose
 $dbname = "umc1";     // Database name
 
